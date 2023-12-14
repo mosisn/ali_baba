418c6b3cd1c9801bfd7e172504686d183a77ed25
Mostafa <mostafa.saneisn@gmail.com>
2023-12-14 10:43:51 +0330

    response change in greeting update

--- alibaba/views.py
+++ alibaba/views.py
@@ -2,7 +2,7 @@ from django.http.response import HttpResponse, JsonResponse
 
 
 def greeting(request):
-    return HttpResponse('welcome to alibaba!')
+    return HttpResponse('welcome to my website!')
 
 
 def about(request):
